e073d1bcfb23309611861385beed550e3c6f87f6
Ting Zhu <ting@merlinz.me>
2020-12-02 13:01:19 +0100

    Revert "Add d2 fileio, fix readme"
    This reverts commit c89886f46a0f02871f3bc83a770d907f41a5624b.

--- meshrcnn/utils/model_zoo.py
+++ meshrcnn/utils/model_zoo.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-from detectron2.utils.file_io import PathHandler, PathManager
+from fvcore.common.file_io import PathHandler, PathManager
 
 __all__ = ["MeshRCNNHandler"]
 
